8e7072bda43e10d7d2802c366739eaf225163a73
totalannihilationitalia <97891425+totalannihilationitalia@users.noreply.github.com>
2022-01-17 22:16:37 +0100

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='War Machines RTS',
+  description='RTS',
+  shortname='WM',
+  version='V0.573b',
+  mutator='Official',
+  game='War Machines RTS',
+  shortGame='WM',
+  modtype=1,
+}
