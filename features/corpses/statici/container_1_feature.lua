6faa1d95da663771744c768d5b22de1cfebeba5b
totalannihilationitalia <totalannihilationitalia@gmail.com>
2025-02-04 20:53:59 +0100

    container feature created

---
+++ features/corpses/statici/container_1_feature.lua
@@ -0,0 +1,23 @@
+return {
+  container_1_feature =
+  {
+    description = "A container",
+    blocking = true,
+    damage = 1000,
+    energy = 0,
+    flammable = false,
+--    featureDead ="pala001d_feature",
+    noselect = false,
+    metal = 250,
+    footprintx = 1,
+    footprintz = 1,
+    object = "container_1.s3o",
+    reclaimable = true,
+    upright = true,
+    crushResistance = 10000,
+               collisionvolumeoffsets = "0 20 0",
+               collisionvolumescales = "60 50 150",
+               collisionvolumetype = "Box",
+  },
+
+}
