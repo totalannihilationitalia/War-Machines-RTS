eb7124bad66b3a703998a48ec15b6baf63aefbca
totalannihilationitalia <totalannihilationitalia@gmail.com>
2025-03-19 21:49:27 +0100

    container now can be transported

--- units/statici/container_1.lua
+++ units/statici/container_1.lua
@@ -5,59 +5,128 @@ return {
 --- UNITS FEATURES
 -----------------------------------------------------------
   container_1= {
-               acceleration = 0,
-               activatewhenbuilt = true,
-               brakerate  = 0,
---               buildangle = 8192,
-               buildcostenergy = 600,
-               buildcostmetal = 54,
-               builder = false,
+               acceleration = 0.0605,
+               brakerate  = 0.2068,
+               buildcostenergy = 1802,
+               buildcostmetal = 128,
+               builddistance = 150,
+               builder = true,
                buildpic = "stazione_radar_1.png",
-               buildtime  = 1137,
-               canAttack = false,
+               buildtime  = 4066,
+               canGuard = true,
+               canmove = true,
+               canPatrol = true,
+--               canreclamate = true,
+--               canstop = 1,
                category = "ALL NOTLAND NOTSUB NOWEAPON NOTSHIP NOTAIR SURFACE OGGETTISTATICI",
                collisionvolumeoffsets = "0 20 0",
                collisionvolumescales = "60 50 150",
                collisionvolumetype = "Box",
---               corpse = "dead",
-               description = "A container",
---               energymake = 4,
---               energystorage = 0,
---               energyUse = 4,
-               explodeas = "SMALL_BUILDINGEX",
+               corpse = "artefatto_sporco_dead",
+--               defaultmissiontype = Standby,
+               description = "Tech Level 1",
+               energymake = 10,
+               energystorage = 50,
+               energyUse = 10,
+               explodeas = "BIG_UNITEX",
                footprintx = 2,
                footprintz = 6,
                icontype = "building",
-               idleautoheal = 0,
+               idleautoheal = 5,
                idletime = 1800,
+--               maneuverleashlength  = 640,
 --               mass = 0 --definire massa,
                maxdamage = 2000,
-               maxslope = 10,
-               maxvelocity = 0,
-               maxwaterdepth = 0,
-               metalStorage = 0,
-               name = "Building",
+               maxslope = 16,
+               maxvelocity = 0.0004,
+               maxwaterdepth = 18,
+               metalmake = 0.1,
+               metalStorage = 50,
+--               mobilestandorders= 1,
+               movementclass = "TANK3",
+               name = "Alien",
                noAutoFire = false,
                objectname = "container_1.s3o",
-               onoffable = true,
-               radardistance = 2100,
+               radardistance = 50,
                seismicsignature = 0,
-               selfdestructas = "SMALL_BUILDING",
---               sightdistance = 680,
---               soundcategory= "ARM_RADAR",
-               TEDClass = "SPECIAL", -- verificare se necessario
-               turnrate = 0,
-               workertime = 0,
-               YardMap= "oooooooooooo",
+               selfdestructas = "BIG_UNIT",
+               sightdistance = 253,
+--               soundcategory= "ARM_CVEHICLE",
+--               steeringmode= 1,
+--               standingmoveorder = 1,
+               TEDClass = "CNSTR", -- verificare se necessario
+               terraformspeed = 270,
+               turnrate = 435,
+               workertime = 90,
+               leaveTracks = true,
+               trackOffset = 12,
+               trackStrength = 6,
+               trackStretch = 1,
+               trackType = "StdTank",
+               trackWidth = 35,
+-----------------------------------
+--- BUILDLIST
+-----------------------------------
+               buildoptions = { 
+               },
 -----------------------------------------------------------
 --- Units wreckage and heaps
 -----------------------------------------------------------
-
+featuredefs = {
+  dead = {
+               world = "All Worlds",
+               description = "unknown artifact Wreckage",
+               category = "corpses",
+               object = "ARMCV_DEAD",
+               featuredead = "armcv_heap",
+               featurereclamate = "SMUDGE01",
+               footprintx = 3,
+               footprintz = 3,
+               height = 20,
+               blocking= true,
+               hitdensity = 100,
+               metal = 83,
+               damage = 744,
+               reclaimable = true,
+               seqnamereclamate = "TREE1RECLAMATE",
+               energy = 0,
+--               collisionvolumeoffsets = ,
+--               collisionvolumescales = ,
+--               collisionvolumetype = ,
+               },  -- Close Dead Features
+  heap = {
+               world = "All Worlds",
+               description = "unknown artifact Heap",
+               category = "heaps",
+               object = "3X3C",
+               footprintx = 3,
+               footprintz = 3,
+               height = 4,
+               blocking = false,
+               hitdensity= 100,
+               metal = 33,
+               damage = 372,
+               reclaimable = true,
+               featurereclamate = "SMUDGE01",
+               seqnamereclamate = "TREE1RECLAMATE",
+               energy = 0,
+               collisionvolumescales = "55.0 4.0 6.0",
+               collisionvolumetype = "cylY",
+               },  -- Close heap
+},  --  Wreckage and heaps
+-----------------------------------------------------------
+--- EFFECTS
+-----------------------------------------------------------
+sfxtypes = {
+  explosiongenerators = {
+               [1]="custom:Nano",
+               }, -- close effects list
+}, -- close section sfxtypes
 -----------------------------------------------------------
 --- UNITS SOUND
 -----------------------------------------------------------
-               sounds = {
-               activate = "radar1",
+sounds = {
+               build = "nanlath1",
                count = {
                      [1] =  "count6",
                      [2] =  "count5",
@@ -67,13 +136,18 @@ return {
                      [6] =  "count1",
                        },
                canceldestruct = "cancel2",
-               deactivate = "radarde1",
+               cant = {
+                     [1] = "cantdo4",
+                      },
+               ok = {
+                     [1] = "varmmove",
+                    },
+               repair = "repair1",
                select = {
-                     [1] = "radar1",
+                     [1] = "varmsel",
                         },
                underattack = "warning1",
-               working = "radar1",
-               },
-                 maxAngleDif = 1,
-               }, 
-               }
+               working = "reclaim1",
+}, --close sound section
+}, -- close unit data 
+} -- close total
