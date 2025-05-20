2fba40dc3ef8ecde1526a11b6390fae168048ceb
totalannihilationitalia <totalannihilationitalia@gmail.com>
2025-05-20 23:20:07 +0200

    unnecessary

--- LuaRules/Gadgets/wmrtsai_config/commander_faction_map.lua
+++
@@ -1,8 +0,0 @@
-  -- Mappa Comandante -> Fazione
-  local commanderFactionMap = {
-      icucom = "ICU",
-      nfacom = "NFA",
-      andcom = "AND",
-      eufcd  = "EUF"
-  }
-  return commanderFactionMap
