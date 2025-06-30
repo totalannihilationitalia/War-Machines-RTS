2aa09d31b7d1c3d72742d3a2731ab41d11368a0a
Dario <97891425+totalannihilationitalia@users.noreply.github.com>
2025-06-30 16:11:32 +0200

    Wip diary for missions

---
+++ LuaUI/Widgets/WMRTS_diary.lua
@@ -0,0 +1,42 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file:    WMRTS_diary.lua
+--  brief:   Diary / Briefing for War Machines RTS missions
+--  author:  Dario Molinaroli ( Molixx81 )
+--
+--  Copyright (C) 2025.
+--  Licensed under the terms of the GNU GPL, v2 or later.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "WMRTS_diary",
+    desc      = "WMRTS diary menu",
+    author    = "molixx81",
+    date      = "30 Giugno, 2025",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true 
+  }
+end
+
+--------------------------------------------------------------------------------
+--[[
+
+to do list 
+-- all :D
+1) Global Map info
+2) Story of War
+3) Hints
+4) Character
+5) Units
+
+Pulsante close
+Pulsante avanti e dietro (per leggere le storie della categoria
+tra i due pulsanti la pagina 1/xx
+Al momento ogni scheda sarà un immagine con nomeimmagine_ing.png (cosi da diversificare quando un domani ci sarà il multilingua).
+
+]]--
