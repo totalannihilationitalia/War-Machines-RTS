7b026aa3d997fa2fed9acc896701e7554d5f159b
totalannihilationitalia <97891425+totalannihilationitalia@users.noreply.github.com>
2025-11-09 23:39:27 +0100

    low poly rock wip

---
+++ features/corpses/statici/lowpoly_rocks.lua
@@ -0,0 +1,25 @@
+return {
+  lowpolyrock001_feature =
+  {
+    description = "A rock",
+    blocking = true,
+    damage = 700,
+    energy = 0,
+    flammable = false,
+--	indestructible = true,
+--    featureDead ="",
+    noselect = true,
+    metal = 100,
+    footprintx = 10,
+    footprintz = 10,
+    object = "lowpoly_rock_001.s3o",
+    reclaimable = false,
+    upright = false,
+--    collisionvolumeoffsets = "0 0 0",
+--    collisionvolumescales = "5 50 5",
+--    collisionvolumetype = "Box",
+  },
+
+
+
+}
