56240717852c13e3491a6cd7e2f4502773a87f71
Dario <97891425+totalannihilationitalia@users.noreply.github.com>
2025-11-12 13:46:01 +0100

    big rock features creation

---
+++ features/corpses/statici/big_rocks_features.lua
@@ -0,0 +1,25 @@
+return {
+  bigrock001_feature =
+  {
+    description = "A rock",
+    blocking = true,
+    damage = 70000,
+    energy = 0,
+    flammable = false,
+    indestructible = true,
+--    featureDead ="",
+    noselect = true,
+    metal = 100,
+    footprintx = 10,
+    footprintz = 10,
+    object = "bigrock001_feature.s3o",
+    reclaimable = false,
+    upright = true,
+--    collisionvolumeoffsets = "0 0 0",
+--    collisionvolumescales = "5 50 5",
+--    collisionvolumetype = "Box",
+  },
+
+
+
+}
