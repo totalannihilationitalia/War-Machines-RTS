eee30663d4af8e7c7cca88ff084aec707d611754
totalannihilationitalia <totalannihilationitalia@gmail.com>
2025-11-18 23:25:36 +0100

    Rock Features WIP

--- features/corpses/statici/big_rocks_features.lua
+++ features/corpses/statici/big_rocks_features.lua
@@ -19,7 +19,29 @@ return {
 --    collisionvolumescales = "5 50 5",
 --    collisionvolumetype = "Box",
   },
-
+  bigrock002_feature =
+  {
+    description = "A rock",
+    blocking = true,
+    damage = 70000,
+    energy = 0,
+    flammable = false,
+    indestructible = true,
+--    featureDead ="",
+    noselect = true,
+    metal = 100,
+    footprintx = 10,
+    footprintz = 10,
+    object = "bigrock002_feature.s3o",
+    reclaimable = false,
+    upright = true,
+--    collisionvolumeoffsets = "0 0 0",
+--    collisionvolumescales = "5 50 5",
+--    collisionvolumetype = "Box",
+  },
 
 
 }
+
+
+
